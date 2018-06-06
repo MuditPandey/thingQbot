53278aa0bee03b5966ebc5a09dc200ccd4bfd0e4
Mudit Pandey <muditpandey@gmail.com>
2018-06-06 11:45:23 +0530

    Added src for libraries that were modified

--- catkin_ws/build/frontier_exploration/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nvidia/catkin_ws/src/frontier_exploration
-# Build directory: /home/nvidia/catkin_ws/build/frontier_exploration
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(_ctest_frontier_exploration_gtest_frontier_search_tests "/home/nvidia/catkin_ws/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/kinetic/share/catkin/cmake/test/run_tests.py" "/home/nvidia/catkin_ws/build/test_results/frontier_exploration/gtest-frontier_search_tests.xml" "--return-code" "/home/nvidia/catkin_ws/devel/lib/frontier_exploration/frontier_search_tests --gtest_output=xml:/home/nvidia/catkin_ws/build/test_results/frontier_exploration/gtest-frontier_search_tests.xml")
-add_test(_ctest_frontier_exploration_gtest_geometry_tools_tests "/home/nvidia/catkin_ws/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/kinetic/share/catkin/cmake/test/run_tests.py" "/home/nvidia/catkin_ws/build/test_results/frontier_exploration/gtest-geometry_tools_tests.xml" "--return-code" "/home/nvidia/catkin_ws/devel/lib/frontier_exploration/geometry_tools_tests --gtest_output=xml:/home/nvidia/catkin_ws/build/test_results/frontier_exploration/gtest-geometry_tools_tests.xml")
-add_test(_ctest_frontier_exploration_gtest_costmap_tools_tests "/home/nvidia/catkin_ws/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/kinetic/share/catkin/cmake/test/run_tests.py" "/home/nvidia/catkin_ws/build/test_results/frontier_exploration/gtest-costmap_tools_tests.xml" "--return-code" "/home/nvidia/catkin_ws/devel/lib/frontier_exploration/costmap_tools_tests --gtest_output=xml:/home/nvidia/catkin_ws/build/test_results/frontier_exploration/gtest-costmap_tools_tests.xml")
--- catkin_ws/devel/lib/python2.7/dist-packages/frontier_exploration/srv/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._BlacklistPoint import *
-from ._GetNextFrontier import *
-from ._UpdateBoundaryPolygon import *
